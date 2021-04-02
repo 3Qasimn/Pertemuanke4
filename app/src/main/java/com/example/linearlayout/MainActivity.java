12fa196b150f33497f48c5d54310c7a7210a1813
3qasimn <3qasimn@gmail.com>
2021-04-02 11:09:54 +0700

    Pesan komit wajib diisi (Commit Message Required) Ex: Commit 1 (publish projek ke dalam github (publish the project to github))

--- app/src/main/java/com/example/linearlayout/MainActivity.java
+++ app/src/main/java/com/example/linearlayout/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.linearlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            Intent i = new Intent(getApplicationContext(), com.example.linearlayout.DaftarActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
